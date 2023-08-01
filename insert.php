259d82fadb1ca0f301cd8221f34d5e182e8c3e47
sakaekomachi <sakae.komachi@mitsubishicorp.com>
2023-08-01 12:46:19 +0900

    0801commit

---
+++ insert.php
@@ -0,0 +1,37 @@
+<?php
+//1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+$name = $_POST['name'];
+$URL = $_POST['URL'];
+$comment = $_POST['comment'];
+
+
+//2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DB Connection Error:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("INSERT INTO gs_bm_table(name,URL,comment,indate)VALUES(:name, :URL, :comment, sysdate());");
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':URL', $URL, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("SQL_ERROR:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: index.php");
+  exit();
+
+}
+?>
